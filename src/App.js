e8340f9570d41b2b76cfff96d29f4ba14a4d4858
Dereje Getahun <getahun.d@gmail.com>
2022-06-09 16:43:41 +0300

    basic MUI collapsble table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import './App.css';
+import React, { Fragment } from 'react';
+import taskListStub from './taskliststub';
+/* MUI */
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-function App() {
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
   return (
-    <div>Hello</div>
+    <Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        {/* <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.fat}</TableCell>
+        <TableCell align="right">{row.carbs}</TableCell>
+        <TableCell align="right">{row.protein}</TableCell> */}
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              {row.description}
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </Fragment>
   );
 }
+
+const App = () => (
+  <TableContainer component={Paper}>
+    <Table aria-label="collapsible table">
+      <TableBody>
+        {taskListStub.map((row) => (
+          <Row key={row.name} row={row} />
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+)
 
 export default App;
---
+++ src/taskliststub.js
@@ -0,0 +1,19 @@
+const taskListStub = [
+    {
+        name: "Task 1",
+        completed: false,
+        description: "This is task # 1"
+    },
+    {
+        name: "Task 2",
+        completed: false,
+        description: "This is task # 2"
+    },
+    {
+        name: "Task 3",
+        completed: false,
+        description: "This is task # 3"
+    }
+]
+
+export default taskListStub;
